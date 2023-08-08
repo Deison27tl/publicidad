0ae5b8f69eacf21e6045cba74215c67acc164318
Deison27tl <82671901+Deison27tl@users.noreply.github.com>
2023-08-08 10:46:14 -0500

    ajute

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-function downloadImage() {
+function generateImage() {
     const nameInput = document.getElementById('nameInput').value;
     const capitalizedFirstLetter = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
     const nameOverlay = document.getElementById('nameOverlay');
     nameOverlay.textContent = `Yo ${capitalizedFirstLetter} creo en ti !`;
 
-    const imageContainer = document.getElementById('imageContainer');
-    const logoImage = document.getElementById('logo');
-
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
-    ctx.font = 'bold 40px "Brush Script MT", cursive';
-    const textWidth = ctx.measureText(nameOverlay.textContent).width;
-
-    const padding = 20; // Espacio alrededor del texto
-    const canvasWidth = textWidth + 2 * padding; // Ancho del texto más el espacio alrededor
+  
+    const canvasWidth = 370; // Ancho del texto más el espacio alrededor
     const canvasHeight = 595; // Alto deseado de la imagen descargada en dispositivos móviles
 
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
+    const logoImage = document.getElementById('logo');
     const img = new Image();
     img.onload = function() {
         ctx.drawImage(logoImage, 0, 0, canvas.width, canvas.height);
+        ctx.font = 'bold 40px "Brush Script MT", cursive';
         ctx.fillStyle = 'white';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
@@ -30,20 +26,23 @@ function downloadImage() {
         ctx.shadowOffsetX = 3;
         ctx.shadowOffsetY = 3;
         ctx.shadowBlur = 5;
-        ctx.fillText(nameOverlay.textContent, canvas.width / 2, canvas.height / 2 + 160);
+        ctx.fillText(nameOverlay.textContent, canvas.width / 2, canvas.height / 2 + 160); // Baja 100 píxeles y mueve 20 píxeles a la derecha
 
-        const imageUrl = canvas.toDataURL('image/png');
-        shareImage(imageUrl); // Llamamos a la función shareImage pasando la URL de la imagen generada
+        const link = document.createElement('a');
+        link.download = 'imagen_personalizada.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+        shareImage(canvas.toDataURL('image/png'));
     };
-    img.src = 'https://barnaby-creoenti.netlify.app/logo.png'; // URL de la imagen del logo
+    img.src = 'https://barnaby-creoenti.netlify.app/logo.png'; // URL real de la imagen de logo
 }
 
 function shareImage(imageUrl) {
     if (navigator.share) {
         navigator.share({
-            url: imageUrl,
             title: 'Imagen personalizada',
             text: '¡Mira la imagen personalizada que he creado!',
+            url: imageUrl,
         })
         .then(() => console.log('Compartido con éxito.'))
         .catch(error => console.log('Error al compartir:', error));
